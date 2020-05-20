8f5cfb08b3096279c5999f72f9b0f117c45f34a5
Zak19018 <sha19018@byui.edu>
2020-05-20 20:54:08 +0200

    added banner message

--- lesson5/js/script.js
+++ lesson5/js/script.js
@@ -8,3 +8,19 @@ var date = today.getDate();
 var month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
 var year = today.getFullYear();
 document.getElementById('currentDate').innerHTML = weekday[today.getDay()] + ", " + date + " " + month[today.getMonth()] + " " + year ;
+
+
+const day = new Date();
+console.log(day);
+
+const dayNumber = day.getDay();
+console.log(dayNumber);
+
+const element = document.getElementById("banner");
+
+if (dayNumber == 5) {
+    element.classList.add("showme");
+}
+else {
+    element.classList.add("hideme");
+}
