e35d7e23136cd0bfb3713caeea7643a51d9a9097
Zak19018 <sha19018@byui.edu>
2020-06-06 18:17:00 +0200

    lesson 7

---
+++ lesson7/js/script.js
@@ -0,0 +1,58 @@
+/* hamburger menu */
+function toggleMenu() {
+    document.getElementById("primaryNav").classList.toggle("hide");
+}
+
+
+/* current date in footer */
+var today = new Date();
+var weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+var date = today.getDate();
+var month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+var year = today.getFullYear();
+document.getElementById('currentDate').innerHTML = weekday[today.getDay()] + ", " + date + " " + month[today.getMonth()] + " " + year ;
+
+
+/* banner message */
+const day = new Date();
+console.log(day);
+
+const dayNumber = day.getDay();
+console.log(dayNumber);
+
+const element = document.getElementById("banner");
+
+if (dayNumber == 6) {
+    element.classList.add("showme");
+}
+else {
+    element.classList.add("hideme");
+}
+
+//lazy loading
+const imagesToLoad = document.querySelectorAll('img[data-src]');
+
+const loadImages = function(image) {
+	image.setAttribute('src', image.getAttribute('data-src'));
+	image.onload = function() {
+		image.removeAttribute('data-src');
+	};
+};
+if('IntersectionObserver' in window) {
+	var observer = new IntersectionObserver(function(items, observer) {
+		items.forEach(function(item) {
+			if(item.isIntersecting) {
+				loadImages(item.target);
+				observer.unobserve(item.target);
+			}
+		});
+	});
+	imagesToLoad.forEach(function(img) {
+		observer.observe(img);
+	});
+}
+else {
+	imagesToLoad.forEach(function(img) {
+		loadImages(img);
+	});
+}
---
+++ lesson7/js/windchill.js
@@ -0,0 +1,21 @@
+/* wind chill */
+    /* get temp & speed from html and convert to numbers */
+    const tempNumber = parseFloat(document.getElementById('temp').textContent);
+    //console.log(tempNumber);
+    const speedNumber = parseFloat(document.getElementById('speed').textContent);
+    //console.log(speedNumber);
+    
+        /* wind chill formula */
+    let windChill = 35.74 + (0.6215 * tempNumber) - (35.75 * Math.pow(speedNumber, 0.16)) + (0.4275 * tempNumber * Math.pow(speedNumber, 0.16));
+    //console.log(windChill);
+    windChill = Math.round(windChill);
+    //console.log(windChill);
+    
+        /* checking */
+    if(tempNumber<=50 && speedNumber>3) {
+        document.getElementById('chill').textContent = "Wind Chill: "+windChill+"\xB0F";
+    }
+        else {
+            document.getElementById('chill').textContent = "N/A";
+        }
+    
