ad27f3f7765ed18c74841cf9f32a5a51d6cf50fb
Zak19018 <sha19018@byui.edu>
2020-07-02 22:35:06 +0200

    content changes for town pages

--- lesson-11/js/script.js
+++ lesson-11/js/script.js
@@ -29,6 +29,35 @@ else {
     element.classList.add("hideme");
 }
 
+
+//lazy loading
+const imagesToLoad = document.querySelectorAll('img[data-src]');
+
+const loadImages = function(image) {
+	image.setAttribute('src', image.getAttribute('data-src'));
+	image.onload = function() {
+		image.removeAttribute('data-src');
+	};
+};
+if('IntersectionObserver' in window) {
+	var observer = new IntersectionObserver(function(items, observer) {
+		items.forEach(function(item) {
+			if(item.isIntersecting) {
+				loadImages(item.target);
+				observer.unobserve(item.target);
+			}
+		});
+	});
+	imagesToLoad.forEach(function(img) {
+		observer.observe(img);
+	});
+}
+else {
+	imagesToLoad.forEach(function(img) {
+		loadImages(img);
+	});
+}
+
 /* json */
 const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
 
